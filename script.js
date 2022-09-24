78b680540a4f003bbc6860e8aac774874588a3e9
https-sam <osgo1115@icloud.com>
2022-09-23 22:55:38 -0600

    highlight

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const themeBlue = "#62BAF3";
 const themeGray = "#6D7989";
-
 const sideItems = document.querySelectorAll(".side-bar-item");
 
-function disableOtherItems(thisItem) {
+function hilightThisItem(thisItem) {
   for (const item of sideItems) {
-    item.style.borderColor = themeGray;
-    item.style.color = themeGray;
-    item.style.fontWeight = "100";
+    if (item === thisItem) {
+      item.style.borderColor = themeBlue;
+      item.style.color = themeBlue;
+      item.style.fontWeight = "bold";
+    } else {
+      item.style.borderColor = themeGray;
+      item.style.color = themeGray;
+      item.style.fontWeight = "100";
+    }
   }
 }
 
 for (const item of sideItems) {
-  item.addEventListener("click", () => {
-    disableOtherItems(item);
-    item.style.borderColor = themeBlue;
-    item.style.color = themeBlue;
-    item.style.fontWeight = "bold";
-  });
+  item.addEventListener("click", () => hilightThisItem(item));
 }
