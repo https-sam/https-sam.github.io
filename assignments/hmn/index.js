087f791094431b5d5a480e15fe898ee66b585a8a
Sam Goto <osgo1115@icloud.com>
2022-10-29 20:32:32 -0600

    Update index.js

--- assignments/hmn/index.js
+++ assignments/hmn/index.js
@@ -1,16 +1,35 @@
-const axios = require("axios");
+>const fetcher = (uri) => {
+    return fetch(uri)
+      .then((res) => res.json())
+      .then((data) => data);
+  };
 
-const fetcher = (uri) => {
-  return axios.get(uri);
-};
+  const employeesDataURI =
+    "https://raw.githubusercontent.com/https-sam/https-sam.github.io/main/assignments/hmn/company.json";
 
-const employeesDataURI =
-  "https://raw.githubusercontent.com/https-sam/https-sam.github.io/main/assignments/hmn/company.json";
+  const logAns = ({ employees }, msg) => {
+    let grossSalary = 0;
+    for (const { salary } of employees) {
+      grossSalary += parseInt(salary);
+    }
+    console.log(`${msg} ${grossSalary}`);
+  };
 
-fetcher(employeesDataURI)
-  .then(({ data }) => {
-    console.log(data);
-    data = JSON.parse(data);
-    console.log(data.companyName);
-  })
-  .catch((e) => console.log(e));
+  const raiseTime = (data) => {
+    for (const employee of data.employees) {
+      if (employee.raiseEligible) {
+        employee.raiseEligible = false;
+        employee.salary *= 1.1;
+      }
+    }
+    return data;
+  };
+
+  fetcher(employeesDataURI)
+    .then((data) => {
+      console.log(data);
+      logAns(data, "Total salary of the employees before raise is:");
+      data = raiseTime(data);
+      logAns(data, "Total salary of the employees after raise is:");
+    })
+    .catch((e) => console.log(e));
