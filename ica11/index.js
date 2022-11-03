ea3530bd07d39a6e8aee81770bcb84375bee3c09
https-sam <osgo1115@icloud.com>
2022-11-02 20:10:50 -0600

    refactor: ica11

--- ica11/index.js
+++ ica11/index.js
@@ -1,24 +1,30 @@
 const API_URI = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
+const ERROR_MESSAGE = "Error Occured during fetching data, Please try again.";
+const QUOTE_CONTAINER = document.getElementById("js-quote-text");
 
 const fetchQuote = () => {
   return new Promise((resolve, reject) => {
     fetch(API_URI)
-      .then((res) => resolve(res.json()))
-      .catch((e) => reject(`Error: ${e}`));
+      .then((res) => res.json())
+      .then((data) => {
+        if (typeof data?.message !== "undefined") resolve(data);
+        else throw new Error(ERROR_MESSAGE);
+      })
+      .catch((e) => reject(e));
   });
 };
 
 const displayQuote = (text) => {
-  document.getElementById("js-quote-text").innerText = text;
+  QUOTE_CONTAINER.innerText = text;
 };
 
 const getQuote = () => {
-  console.log("Fetching a new quote");
+  console.log("Fetching a new quote . . .");
   fetchQuote()
-    .then((data) => {
-      console.log(data);
-      displayQuote(data?.message);
-      localStorage.setItem("quote", data?.message);
+    .then(({ message }) => {
+      console.log(message);
+      displayQuote(message);
+      localStorage.setItem("quote", message);
     })
     .catch((e) => {
       console.log(e);
