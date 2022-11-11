5e5e2d850d41cd3100f4eea37a440da6249e0d57
https-sam <osgo1115@icloud.com>
2022-11-10 22:09:06 -0700

    refactor: wa12

--- assignments/12/index.js
+++ assignments/12/index.js
@@ -2,6 +2,15 @@ const keys = Array.from(document.querySelectorAll(".phone-key"));
 const enteredNum = document.getElementById("phone-number");
 const deleteBtn = document.getElementById("delete-btn");
 const converBtn = document.getElementById("convert-btn");
+const BINARY = /^[01]*$/;
+
+function isValuidBinary(val) {
+  if(!BINARY.test(enteredNum.innerHTML)) {
+    enteredNum.innerHTML = val;
+    return false;
+  }
+  return true
+}
 
 deleteBtn.addEventListener("click", () => {
   const num = enteredNum.innerHTML;
@@ -12,14 +21,18 @@ deleteBtn.addEventListener("click", () => {
   );
 });
 
+
 converBtn.addEventListener("click", () => {
-  enteredNum.innerHTML = BigInt('0b' + enteredNum.innerHTML)
+  if( !isValuidBinary("") || enteredNum.innerHTML.length === 0) return;
+  else enteredNum.innerHTML = BigInt('0b' + enteredNum.innerHTML)
 })
 
 for (const key of keys) {
   const val = parseInt(key.innerHTML);
   key.addEventListener("click", () => {
-    if (isNaN(val)) return;
-    enteredNum.innerText += val;
+    if(isValuidBinary(key.innerHTML)) {
+      if (isNaN(val)) return;
+      enteredNum.innerText += val;
+    }
   });
 }
